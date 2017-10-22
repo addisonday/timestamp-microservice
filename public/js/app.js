cb775ade4d6ce5812997d1309931f4e51fe9160d
addisonday <day.addison@yahoo.com>
2017-10-22 14:44:45 -0700

    Added views directory & other refactoring

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+angular.module('timestamp', ['appRoutes', 'MainCtrl']);
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,3 +1,5 @@
 angular.module('MainCtrl', []).controller('MainController', function MainCtrl() {
+  var dateVal = req.params.dateVal;
 
+  res.json({unix: dateVal});
 });
