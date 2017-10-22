d2b897158b7e72e222359fe504dcaa9270a57e7a
addisonday <day.addison@yahoo.com>
2017-10-22 13:45:44 -0700

    Adding routes, and controller

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,11 @@ module.exports = function(app) {
 
   // server routes ===========================================================
   // handle things like api calls
-  // authentication routes
+  app.get('/:dateVal', function(req, res, next){
+    var dateVal = req.params.dateVal;
+
+    res.json({unix: dateVal});
+  });
 
   // frontend routes =========================================================
   // route to handle all angular requests
