abed34f271f6ea67f9a8f4a9f5e94b26ef187c57
addisonday <day.addison@yahoo.com>
2017-10-22 21:16:07 -0700

    added server-side logic and updated index.html with examples

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,23 @@ module.exports = function(app) {
   app.get('/:dateVal', function(req, res, next){
     var dateVal = req.params.dateVal;
 
-    res.json({unix: dateVal});
+    var dateFormat = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+
+    if(isNaN(dateVal)){
+      var naturalDate = new Date(dateVal);
+      naturalDate = naturalDate.toLocaleString("en-us", dateFormat);
+      var unixDate = new Date(dateVal).getTime()/1000;
+    } else{
+      var unixDate = dateVal;
+      var naturalDate = new Date(dateVal * 1000)
+      naturalDate = naturalDate.toLocaleString("en-us", dateFormat);
+    }
+
+    res.json({unix: unixDate, natural: naturalDate});
   });
 
   // frontend routes =========================================================
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,2 @@
-angular.module('MainCtrl', []).controller('MainController', function MainCtrl() {
-  var dateVal = req.params.dateVal;
-
-  res.json({unix: dateVal});
+angular.module('MainCtrl', []).controller('MainController', function MainCtrl($scope) {
 });
